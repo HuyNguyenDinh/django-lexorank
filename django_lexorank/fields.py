d019aca45f1a2bf6c028e700c943759b2aa00c04
Dinh Huy <huynd86@fpt.com>
2024-12-16 14:39:02 +0700

    [+] Add list support for order_with_respect_to

--- django_lexorank/fields.py
+++ django_lexorank/fields.py
@@ -18,15 +18,17 @@ class RankField(models.CharField):
 
         if not current_rank:
             model = model_instance._meta.model
-
+            with_respect_to_kwargs = {}
             if model.order_with_respect_to:
-                with_respect_to_kwargs = {
-                    model.order_with_respect_to: getattr(
-                        model_instance, model.order_with_respect_to
-                    )
-                }
-            else:
-                with_respect_to_kwargs = {}
+                # with_respect_to_kwargs = {
+                #     model.order_with_respect_to: getattr(
+                #         model_instance, model.order_with_respect_to
+                #     )
+                # }
+                for field in model.order_with_respect_to:
+                    with_respect_to_kwargs.update({
+                        field: getattr(model_instance, field)
+                    })
 
             if self.insert_to_bottom:
                 previous_rank = model.get_last_object_rank(
